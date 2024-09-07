68fefaf3a688f68f90cc188c573e37f88c9b741e
Eclypse-Prime <arthur.liu.lsh@protonmail.com>
2024-09-08 01:57:13 +0200

    refactor: updated gitignore

--- script_example.sh
+++ script_example.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cargo build --release
-target/release/pijersi-toolbox new -e 3 -o positions/positions --split 100
+target/release/pijersi-toolbox new -e 3 -o data/positions/positions --split 100
 for i in {0..99}
 do
-target/release/pijersi-toolbox load positions/positions_$i positions -s 1 -o responses/responses_$i
+target/release/pijersi-toolbox load data/positions/positions_$i positions -s 1 -o data/responses/responses_$i
 done
