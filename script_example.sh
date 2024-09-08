46a2450398a4be6b3304767104ecfe7c891a6f69
Eclypse-Prime <arthur.liu.lsh@protonmail.com>
2024-09-08 12:08:08 +0200

    feat: updated script examples

--- script_example.sh
+++ script_example.sh
@@ -3,5 +3,9 @@ cargo build --release
 target/release/pijersi-toolbox new -e 3 -o data/positions/positions --split 100
 for i in {0..99}
 do
-target/release/pijersi-toolbox load data/positions/positions_$i positions -s 1 -o data/responses/responses_$i
+target/release/pijersi-toolbox load data/positions/positions_$i positions -s 1 -o data/responses/responses_$i 1> /dev/null
 done
+target/release/pijersi-toolbox merge data/responses/responses responses -o data/responses/openings_3 -n 100
+load data/responses/openings_3 responses -e 3 -s 1 -o data/responses/openings_2
+load data/responses/openings_3 responses -e 2 -s 2 -o data/responses/openings_1
+load data/responses/openings_3 responses -e 1 -s 3 -o data/responses/openings_0
